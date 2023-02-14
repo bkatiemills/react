359203dd2c67e163e11d1eca5287e202efac20da
billmills <you@example.com>
2023-02-14 15:22:36 -0500

    tests

--- argovis/src/pages/url.test.js
+++ argovis/src/pages/url.test.js
@@ -15,12 +15,8 @@ describe("your test suite", () => {
         'polygon': [[0,0],[0,1],[1,1],[1,0],[0,0]]
     }
 
-    let mockObject = {
-        'apiPrefix': 'https://example.com/'
-    }
-
     test("Check temporospatial URL", () => {
-        expect(helpers.generateTemporoSpatialURL.bind(mockObject)(this.apiPrefix, 'argo',mockstate)).toBe("https://example.com/argo?compression=minimal&presRange=100,20000&startDate=2023-01-01T00:00:00Z&endDate=2023-01-03T00:00:00Z&polygon=[[0,0],[0,1],[1,1],[1,0],[0,0]]");
+        expect(helpers.generateTemporoSpatialURL.bind(null)('https://example.com/', 'argo',mockstate)).toBe("https://example.com/argo?compression=minimal&presRange=100,20000&startDate=2023-01-01T00:00:00Z&endDate=2023-01-03T00:00:00Z&polygon=[[0,0],[0,1],[1,1],[1,0],[0,0]]");
     });
 
     // argo
